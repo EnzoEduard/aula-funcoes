1dfdda43cd1f405af9dbeba74085fa2abe1726bc
enzo <142436206+EnzoEduard@users.noreply.github.com>
2023-08-31 17:11:39 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,77 @@
+// interpretação de código
+/* exercicio 1: 
+A: sera impresso console o resultado 50, 10
+B: n sera impresso absolutamente nada dentro do console
+*/
+/* exercicio 2: 
+A: ela serve para perguntar se existe a palavra cenoura na string fornecida
+B: sera retornado ao console
+true
+true
+false
+*/
+// escrita de código
+
+// exercicio 1:
+/*A:
+function informaçaoDoUsuario(){
+    console.log("Eu sou Enzo, " + "tenho 16 anos, " + "moro em São Leopoldo e sou estudante.");
+}
+informaçaoDoUsuario();
+*/
+/*
+B:
+function imprimirinfo(nome,idade,endereço,profissao) {
+    return "eu sou " + nome, "tenho " + idade + " anos, " + "moro em " + endereço + " e sou " + profissao;
+}
+console.log(imprimirinfo('Enzo', 16, 'SL', 'estudante'));
+*/
+// exercicio 2:
+/*
+a
+function somaNumeros(numero1, numero2){
+return numero1 + numero2;
+}
+console.log(somaNumeros(5,7));
+*/ 
+/*
+b
+function maiorOuIgual(numero1, numero2) {
+    return (numero1 >= numero2
+}
+console.log(meioOuIgual(4,6));
+ */
+/*
+c
+function verificaImpar(numero1){
+    return (numero1 % 2 === 0);
+}
+console.log(verificaImpar(9));
+*/
+/*
+d
+function tamanhoMaiuscula(msg){
+return msg.length + " " + msg.toUpperCase()
+}
+console.log(mensagem(prompt("lklkklkç"))); 
+*/
+// exercicio 3
+/*
+function soma (soma1,soma2){
+return soma1 + soma2
+}
+
+function subtraçao (subtraçao1,subtraçao2){
+    return subtraçao1 - subtraçao2
+}
+
+function multiplicaçao (multiplicaçao1 , multiplicaçao2) {
+    return multiplicaçao1 * multiplicaçao2
+}
+function divisao (divisao1 , divisao2){
+    return divisao1 / divisao2
+}
+let num1 = Number(prompt("digite um numero para teste"));
+let num2 = Number(prompt("digite um segundo numero"));
+console.log(soma(num1,num2), subtraçao(num1,num2), multiplicaçao(num1,num2), divisao(num1,num2));
+*/
